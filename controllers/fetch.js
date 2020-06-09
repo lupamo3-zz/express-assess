5293b484eeed9524285d2850976bb055d9251051
Norbert Lupamo Anjichi <lupamo93@gmail.com>
2020-06-09 16:27:40 +0300

    Push

--- controllers/fetch.js
+++ controllers/fetch.js
@@ -2,48 +2,48 @@ var json_obj = require('../json/fetch.json');
 
 
 // Welcome message
-exports.home = async (req, res) =>{
-    try{
-        await res.send('Hello, welcome!');
-    } catch (error){
-        res.status('404').json({message: error.message});
+exports.home = async (req, res) => {
+    try {
+        return res.send('Hello, welcome!');
+    } catch (error) {
+        return res.status('404').json({ message: error.message });
     }
 };
 
 // Select all Users
-exports.fetch_all = async (req, res) =>{
-    try{
+exports.fetch_all = async (req, res) => {
+    try {
         console.log(json_obj);
-        await res.status('200').json({
-              success: 'true',
-              message: 'All users fetched succesfully',
-              json_obj
+        return res.status('200').json({
+            success: 'true',
+            message: 'All users fetched succesfully',
+            json_obj
         });
     }
     catch (error) {
-        res.status('500').json({message: error.message});
+        return res.status('500').json({ message: error.message });
     }
 };
 
 
 //Select single user
 exports.fetch_one = async (req, res) => {
-    try{
+    try {
         const user_fetch = req.params.user_id;
         console.log("show user", user_fetch);
-        if (user_fetch >= 1 && user_fetch <= 10){
-        res.status('200').json({
-            success: 'true',
-            message: `User ID ${user_fetch} has been fetched`,
-            js: json_obj.filter(function(obj) {return obj.id == req.params.user_id})
+        if (user_fetch >= 1 && user_fetch <= 10) {
+            return res.status('200').json({
+                success: 'true',
+                message: `User ID ${user_fetch} has been fetched`,
+                js: json_obj.filter(function (chunga) { return chunga.id == req.params.user_id; })
+            });
+        }
+        return res.status('404').json({
+            success: 'false',
+            message: `No user with ID ${user_fetch} found`
         });
     }
-    return res.status('404').json({
-        success: 'false',
-        message: `No user with ID ${user_fetch} found`
-    });
-    }
     catch (error) {
-        res.status('500').json({message: error.message});
+        return res.status('500').json({ message: error.message });
     }
 };
--- server.js
+++ server.js
@@ -2,25 +2,21 @@
 var express = require('express');
 var app = express();
 
-require('dotenv').config();
-
 //import routes
 const routes = require('./routes/routes');
 
 const port = 5000 | process.env.PORT;
 app.use('/secure', routes);
-app.use('/', routes);
 
-app.use((req, res, next)=>{
-    const error = new Error('Invalid endpoint, no resource on that route');
-    error.status = 404;
-    next(error);
+
+app.use((req, res, next) => {
+    res.status(404).json('Invalid endpoint, no resource on that route');
 });
 
 app.use((error, req, res, next) => {
     res.status(error.status || 500);
 });
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('Server is running on port ' + port);
 });
